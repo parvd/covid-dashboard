b9f0d279ac2b6ba63d02553713de437350405d4c
Parv Dave <parvd5@gmail.com>
2020-07-27 23:53:47 +0530

    Updates

---
+++ precache-manifest.c555518288a91cda3655779496672b64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c77f994987aedddf109c1d564c08d8",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "0e73e2a0e8511936c482",
+    "url": "/covid-dashboard/static/css/main.0816113a.chunk.css"
+  },
+  {
+    "revision": "0a4d7f1f23ee458ba91d",
+    "url": "/covid-dashboard/static/js/2.d3498bd1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-dashboard/static/js/2.d3498bd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e73e2a0e8511936c482",
+    "url": "/covid-dashboard/static/js/main.4c86374f.chunk.js"
+  },
+  {
+    "revision": "aae4bc047a0b88ce9660",
+    "url": "/covid-dashboard/static/js/runtime-main.697d0302.js"
+  },
+  {
+    "revision": "6ce9074d039177a5b09ef64551377167",
+    "url": "/covid-dashboard/static/media/covid19_logo.6ce9074d.png"
+  }
+]);
